6efb4d8ae8e307ba3f9219ce9cb1f0c1ff6a35bc
SteinarLaenen <steinar9@gmail.com>
2018-06-14 14:28:26 +0200

    Changed organization of the data

--- main.py
+++ main.py
@@ -32,36 +32,6 @@ class Net(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
-def train(args, model, device, train_loader, optimizer, epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
-
-def test(args, model, device, test_loader):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, size_average=False).item() # sum up batch loss
-            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
 
 def main():
     # Training settings
@@ -94,7 +64,6 @@ def main():
     train_set = dset.MNIST(root='./mnist', train=True, transform=trans)
     test_set = dset.MNIST(root='./mnist', train=False, transform=trans)
 
-    batch_size = 50
 
     #divide the set to training and validation
     from torch.utils.data.sampler import SubsetRandomSampler
@@ -110,12 +79,12 @@ def main():
     
     train_loader = torch.utils.data.DataLoader(
         dataset=train_set,
-        batch_size=batch_size,                 
+        batch_size= args.batch_size,                 
         sampler=train_sampler)
 
     valid_loader = torch.utils.data.DataLoader(
         dataset=train_set,
-        batch_size=batch_size,                 
+        batch_size= args.batch_size,                 
         sampler=valid_sampler)
     
 
@@ -133,8 +102,39 @@ def main():
 
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader)
+        val(args, model, device, valid_loader)
 
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset) - 6000,
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def val(args, model, device, val_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in val_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, size_average=False).item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(val_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(val_loader.dataset),
+        100. * correct / len(val_loader.dataset)))        
 
 if __name__ == '__main__':
     main()
