a093ebbeb7d8516cbf8874b81785cb8b085db628
Berend Jansen <Berend@wcw-stud-145-109-16-40.wireless.uva.nl>
2018-06-15 10:26:12 +0200

    steinar is stupid

--- main.py
+++ main.py
@@ -206,7 +206,7 @@ def test(args, model, device, test_loader, criterion, filename):
             file.write('\n')    
 
 def adjust_learning_rate(args, optimizer, epoch):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    """Sets the learning rate to the initial LR decayed by 10 every 10 epochs"""
     lr = args.lr * (0.1 ** (epoch // 10))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr            
